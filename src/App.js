5b8bb3a59aca281a33c378b3af1c496ef10ad3b5
sumanth vanacharla <sumanthvanacharla@pop-os.localdomain>
2024-06-02 03:18:00 -0500

    expenses first comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Dashboard from "./Screens/Dashborad";
+import Expense from "./Screens/Expense";
+import Income from "./Screens/Income";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous"
+        ></link>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+          integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+          crossorigin="anonymous"
+        ></script>
+        <div className="d-flex">
+          <div className="navigation">
+            <Navigation />
+          </div>
+          <Routes>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/expense" element={<Expense />}></Route>
+            <Route path="/income" element={<Income />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Screens/Dashborad.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashborad() {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashborad
